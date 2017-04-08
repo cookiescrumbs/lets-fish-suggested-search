654a175dd3a0e861855695f585064e62d643cac7
Steven <stevenjamescook@gmail.com>
2017-04-08 10:24:07 +0100

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import items from './data/items';
+import { SuggestedSearch } from 'barbel';
+
+const props = {
+    items,
+    label: 'Search for fishing',
+    placeholder: 'Search for fly fishing',
+    errorMessage: 'Custom error message: {{search}}'
+};
+
+render(
+    <SuggestedSearch { ...props }/>,
+     document.querySelector('#suggested-search')
+);
