7134bbc2d666e3df93085dfff2a9a2288c685cb8
Steven <stevenjamescook@gmail.com>
2017-07-26 00:01:35 +0100

    added ajax request

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,44 @@
 import React from 'react';
 import { render } from 'react-dom';
-import items from './data/items';
+import axios from 'axios';
 import { SuggestedSearch } from 'barbel';
 
-const props = {
-    items,
-    label: 'start your search',
-    placeholder: 'start your search',
-    errorMessage: 'We couldn’t find results for {{search}}'
-};
+
+export default class LetsFishSuggestedSearch extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: null,
+            label: 'Start your search',
+            placeholder: 'Start your search',
+            errorMessage: 'We couldn’t find results for {{search}}'
+        };
+    }
+
+    componentDidMount() {
+        axios.get('/search/suggested', {
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then((res) => {
+            this.setState({ items: res.data });
+        });
+    }
+
+    render() {
+        return (
+        <div>
+            <SuggestedSearch { ...this.state }/>
+        </div>
+        );
+    }
+
+}
 
 render(
-    <SuggestedSearch { ...props }/>,
-     document.querySelector('#suggested-search')
+    <LetsFishSuggestedSearch />,
+    document.querySelector('#suggested-search')
 );
